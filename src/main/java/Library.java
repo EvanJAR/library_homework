0382318a796c368fefc877aeb182b482490f01bd
Evan Reid <evan_1995@hotmail.com>
2021-01-26 21:28:00 +0000

    can count number of books, can add a book

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> books;
+
+    public Library(){
+        this.books = new ArrayList<>();
+    }
+
+    public int countBooks(){
+        return this.books.size();
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,14 +1,33 @@
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class LibraryTest {
 
     private Library library;
-    private Book book;
+    private Book book1;
+    private Book book2;
+    private Book book3;
 
     @Before
     public void before(){
         library = new Library();
-        book = new Book("On Beauty", "Zadie Smith", "Literary Fiction");
+        book1 = new Book("On Beauty", "Zadie Smith", "Literary Fiction");
+        book2 = new Book("Infinite Jest", "David Foster Wallace", "Literary Fiction");
+        book3 = new Book("Game of Thrones", "George R. R. Martin", "Fantasy");
+    }
+
+    @Test
+    public void canCountBooks(){
+        Assert.assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBook(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        Assert.assertEquals(3, library.countBooks());
     }
 
 }
